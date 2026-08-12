c0a8b3313314fcafe626a8c1e9098b54c9c5cc61
weceboy <wecarebook@gmail.com>
2026-08-12 13:34:52 +0200

    feat: load workflow folder drag and drop

--- folder-state.js
+++ folder-state.js
@@ -17,4 +17,5 @@
   const nodeModelScript=document.createElement("script");nodeModelScript.src="./node-model.js";nodeModelScript.async=false;document.body.appendChild(nodeModelScript);
   const folderUiScript=document.createElement("script");folderUiScript.src="./folder-ui.js";folderUiScript.async=false;document.body.appendChild(folderUiScript);
   const promptNavScript=document.createElement("script");promptNavScript.src="./prompt-nav.js";promptNavScript.async=false;document.body.appendChild(promptNavScript);
+  const dragDropScript=document.createElement("script");dragDropScript.src="./folder-dragdrop.js";dragDropScript.async=false;document.body.appendChild(dragDropScript);
 })();
